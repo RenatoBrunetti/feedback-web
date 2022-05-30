5f81e216e383939a5097f140156c3ecdc755fddd
Renato Brunetti <recarapia@gmail.com>
2022-05-29 22:42:11 -0300

    Add full mobile project

---
+++ mobile/src/components/Options/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_primary,
+    marginBottom: 32
+  },
+  options: {
+    marginBottom: 48,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+});
